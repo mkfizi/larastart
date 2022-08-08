5f8097002df38c9cfbe3f647af40d95847080e83
mkfizi <mkfizi93@gmail.com>
2022-08-09 06:58:33 +0800

    @ web: ^home view

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -1,13 +1,11 @@
 <x-web-layout>
-    <div class="max-w-2xl m-auto">
-        <div class="mb-4">
+    <div class="max-w-2xl m-auto mt-16">
+        <div class="mb-8">
             <x-heading-1 class="font-extrabold sm:text-5xl lg:text-6xl">Quickstart Your Laravel Applications</x-heading-1>
         </div>
-        <div class="mb-8">
+        <div class="mb-16">
             <x-text class="text-lg text-slate-4600 dark:text-slate-400">Larastart is a Laravel 9 web application scaffolding with built-in features and components stylized with TailwindCSS.</span></x-heading-2>
         </div>
-        <div class="p-4 font-mono bg-gray-200 rounded-md dark:bg-gray-700">
-            composer create-project mkfizi/larastart new-app
-        </div>
+        <x-text class="p-4 font-mono bg-gray-200 rounded-md dark:bg-gray-700">composer create-project mkfizi/larastart new-app</x-text>
     </div>
 </x-web-layout>
