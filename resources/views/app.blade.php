50f8434114ccbeca0765e61820e3040a6c8b2a51
mkfizi <mkfizi93@gmail.com>
2022-08-10 01:23:37 +0800

    % layout: ^app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,11 +6,17 @@
     @include('layouts.partials.twitter-card')
     @include('layouts.partials.styles')
     @include('layouts.partials.scripts')
-        
-    <title>{{ isset($_pageTitle) ? $_pageTitle .' | ' . __('Larastart') : __('Larastart') }}</title>
+     
+    {{-- If $pageTitle in layout exists --}}
+    @if(!empty($attributes['pageTitle']))
+        <title>{{ $attributes['pageTitle'] .' | ' . __('Larastart') }}</title>
+    @else
+        <title>{{ __('Larastart') }}</title>
+    @endif
+    
 </head>
 
-<body class="relative min-h-screen antialiased text-gray-900 dark:text-white {{ isset($_backgroundColor) ? $_backgroundColor  : 'bg-white' }} dark:bg-gray-900">
+<body class="relative min-h-screen antialiased text-gray-900 dark:text-white {{ isset($bgColor) ? $bgColor  : 'bg-white' }} dark:bg-gray-900">
     
     {{-- Page Layout --}}
     @yield('layout')
