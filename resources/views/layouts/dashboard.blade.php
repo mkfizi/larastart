4a11cc73f683e469aa093ced66831869d8c302b6
mkfizi <mkfizi93@gmail.com>
2022-08-10 01:55:39 +0800

    % layout: ^dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,14 +1,22 @@
 @extends('app')
 
+@php
+    $bgColor = 'bg-gray-50';
+@endphp
+
 @section('layout')
     <div class="flex">
         @include('layouts.partials.dashboard.sidebar')
-        <main>
-            {{-- @include('layouts.partials.web.navbar') --}}
-            <div class="container px-4 py-16 mx-auto">
-                {{ $slot }}
-            </div>
-        </main>
+
+        <div class="flex-auto w-full">
+            @include('layouts.partials.dashboard.navbar')
+            <main class="container p-4 mx-auto lg:px-6">
+                <section class="mt-4 mb-16">
+                    <p class="mb-4 text-2xl font-semibold">{{ !empty($attributes['pageTitle']) ? $attributes['pageTitle'] : '' }}</p>
+                    {{ $slot }}
+                </section>
+            </main>
+            @include('layouts.partials.dashboard.footer')
+        </div>
     </div>
-    {{-- @include('layouts.partials.web.footer') --}}
 @endsection
