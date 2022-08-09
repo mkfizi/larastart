64a01f04dd3ea28ce1cc9ae6538153c5089f7a72
mkfizi <mkfizi93@gmail.com>
2022-08-10 01:56:55 +0800

    % web: ^footer

--- resources/views/layouts/partials/web/footer.blade.php
+++ resources/views/layouts/partials/web/footer.blade.php
@@ -1,13 +1,3 @@
 <footer class="absolute bottom-0 w-full mb-1 text-center">
-    <x-text><small>Larastart © <span id="footerCurrentYear"></span></small></x-text>
-    {{-- 
-        Handle footer current year.
-
-        These codes below are called here instead of being inside app.js to 
-        avoid FOUC should app.js type is defined as a module. 
-        --------------------------------------------------------------------------}}
-    <script>
-        const element = document.getElementById("footerCurrentYear");
-        element.innerHTML = new Date().getFullYear();
-    </script>
+    <x-text><small>{{ __('Larastart') }} © {{ date('Y')}}</small></x-text>
 </footer>
