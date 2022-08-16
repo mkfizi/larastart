6e0d88a8af81fe424cff469c2c898af1391248a1
mkfizi <mkfizi93@gmail.com>
2022-08-17 01:09:50 +0800

    % component: ^link class attribute

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,3 +1,11 @@
-<a {{ $attributes->merge(['class' => 'inline-block mb-1 text-sm transition text-blue-600 dark:text-blue-400 hover:text-blue-500 dark:hover:text-blue-500']) }}>
+<a {{ 
+    $attributes->class([
+        'mb-1', 
+        'text-sm', 
+        'inline-block', 
+        'text-blue-600 dark:text-blue-400 hover:text-blue-500 dark:hover:text-blue-500', 
+        'transition',
+    ]) 
+}}>
     {{ $slot }}
 </a>
