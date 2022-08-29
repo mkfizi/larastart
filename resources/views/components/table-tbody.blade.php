8f6bae3d1a8242ffac09df35c97b709e8e19aa95
mkfizi <mkfizi93@gmail.com>
2022-08-30 02:37:09 +0800

    % components: ^table config

--- resources/views/components/table-tbody.blade.php
+++
@@ -1,7 +0,0 @@
-<tbody {{ 
-    $attributes->class([
-        'divide-y divide-gray-200 dark:divide-gray-600',
-    ]) 
-}}>
-    {{ $slot }}
-</tbody>
--- resources/views/components/table-thead.blade.php
+++
@@ -1,9 +0,0 @@
-<thead {{ 
-    $attributes->class([
-        'text-left',
-        'bg-gray-100 dark:bg-gray-700',
-        'border-b border-gray-200 dark:border-gray-800'
-    ]) 
-}}>
-    {{ $slot }}
-</thead>
