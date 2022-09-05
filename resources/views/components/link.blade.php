97c19e869d166444a736631d5e6aa2f576b0cb44
mkfizi <mkfizi93@gmail.com>
2022-09-05 10:31:39 +0800

    % components: ^link

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,10 +1,22 @@
+@props([
+    'underline' => false
+])
+
+@php
+    $underlineClass = '';
+    if ($underline == true) {
+        $underlineClass = 'underline';
+    }
+@endphp
+
 <a {{ 
     $attributes->class([
         'mb-1', 
         'text-sm', 
         'inline-block', 
+        $underlineClass, 
         'text-blue-600 dark:text-blue-400 hover:text-blue-500 dark:hover:text-blue-500', 
-        'transition',
+        'transition', 
     ]) 
 }}>
     {{ $slot }}
