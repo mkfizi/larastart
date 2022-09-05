f49891d4ee2f27ac9ceb9fb9da5f2c6119f1046a
mkfizi <mkfizi93@gmail.com>
2022-09-05 10:31:52 +0800

    % components: ^list

--- resources/views/components/list-decimal.blade.php
+++
@@ -1,8 +0,0 @@
-<ul {{ 
-    $attributes->class([
-        'mb-1 ml-4', 
-        'list-decimal', 
-    ]) 
-}}>
-    {{ $slot }}
-</ul>
--- resources/views/components/list.blade.php
+++ resources/views/components/list.blade.php
@@ -1,6 +1,28 @@
+@props([
+    'type' => ''
+])
+
+@php
+    $marginLeft = 'ml-4';
+    
+    switch ($type) {
+        case 'disc':
+            $listType = 'list-disc';
+            break; 
+        case 'decimal':
+            $listType = 'list-decimal';
+            break; 
+        default:
+            $listType = null;
+            $marginLeft = null;
+            break;
+    }
+@endphp
+
 <ul {{ 
     $attributes->class([
-        'mb-1', 
+        "mb-1 $marginLeft", 
+        $listType,
     ]) 
 }}>
     {{ $slot }}
