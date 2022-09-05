ab7b4c560e294432d07ba72ed6a6a88e9fc4f766
mkfizi <mkfizi93@gmail.com>
2022-09-05 14:48:07 +0800

    % components: ^text validation type

--- resources/views/components/text-invalid.blade.php
+++
@@ -1,9 +0,0 @@
-<p {{ 
-    $attributes->class([
-        'mb-1', 
-        'text-xs', 
-        'text-red-700 dark:text-red-300'
-    ]) 
-}}>
-    {{ $slot }}
-</p>
--- resources/views/components/text-valid.blade.php
+++
@@ -1,9 +0,0 @@
-<p {{ 
-    $attributes->class([
-        'mb-1', 
-        'text-xs', 
-        'text-green-700 dark:text-green-300'
-    ]) 
-}}>
-    {{ $slot }}
-</p>
