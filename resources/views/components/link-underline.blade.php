794017cc6e41c5543d1b68e695bf54e123d7fbc2
mkfizi <mkfizi93@gmail.com>
2022-09-15 12:14:57 +0800

    % component: ^link rename tag

--- resources/views/components/link-underline.blade.php
+++ resources/views/components/link-underline.blade.php
@@ -5,6 +5,7 @@
         'inline-block', 
         'text-blue-600 dark:text-blue-400 hover:text-blue-500 dark:hover:text-blue-500', 
         'transition', 
+        'underline'
     ]) 
 }}>
     {{ $slot }}
