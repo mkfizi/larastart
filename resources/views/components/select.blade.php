3692fbe39f50a293a63ffa9e1186ef712d59b0cc
mkfizi <mkfizi93@gmail.com>
2022-09-15 13:16:00 +0800

    % component: ^sort class position

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,16 +1,16 @@
 <select {{ 
     $attributes->class([
-        'mb-1', 
-        'text-sm', 
+        'h-10',
+        'mb-1',
+        'px-2',
         'w-full',
-        'h-10', 
-        'px-2', 
-        'bg-transparent disabled:bg-gray-100 dark:disabled:bg-gray-700', 
-        'border border-gray-400 focus:border-blue-500 dark:border-gray-600', 
-        'focus:ring-blue-500',
-        'focus:outline-none',
+        'text-sm',
         'rounded',
-        'appearance-none'
+        'appearance-none',
+        'focus:outline-none',
+        'focus:ring-blue-500',
+        'bg-transparent disabled:bg-gray-100 dark:disabled:bg-gray-700',
+        'border border-gray-400 focus:border-blue-500 dark:border-gray-600',
     ]) 
 }}>
     {{ $slot }}
--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,15 +1,15 @@
 <textarea {{ 
     $attributes->class([
-        'mb-1', 
-        'text-sm', 
-        'w-full', 
-        'px-2', 
-        'bg-transparent disabled:bg-gray-100 dark:disabled:bg-gray-700', 
-        'border border-gray-400 focus:border-blue-500 dark:border-gray-600', 
-        'focus:ring-blue-500',
-        'focus:outline-none',
+        'mb-1',
+        'px-2',
+        'w-full',
+        'text-sm',
         'rounded',
-        'appearance-none'
+        'appearance-none',
+        'focus:outline-none',
+        'focus:ring-blue-500',
+        'bg-transparent disabled:bg-gray-100 dark:disabled:bg-gray-700',
+        'border border-gray-400 focus:border-blue-500 dark:border-gray-600',
     ]) 
 }}
     {{ $slot }}
