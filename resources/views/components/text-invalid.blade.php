0ea3a291fb800fa0220011542e9bf15d4ef6dc0e
mkfizi <mkfizi93@gmail.com>
2022-09-15 13:16:44 +0800

    % component: ^text validation rename tag

---
+++ resources/views/components/text-invalid.blade.php
@@ -0,0 +1,9 @@
+<p {{ 
+    $attributes->class([
+        'mb-1',
+        'text-xs',
+        'text-red-700 dark:text-red-300',
+    ]) 
+}}>
+    {{ $slot }}
+</p>
---
+++ resources/views/components/text-valid.blade.php
@@ -0,0 +1,9 @@
+<p {{ 
+    $attributes->class([
+        'mb-1',
+        'text-xs',
+        'text-green-700 dark:text-green-300',
+    ]) 
+}}>
+    {{ $slot }}
+</p>
