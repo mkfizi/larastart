78c42f7088b796f0c35f36c45c27a7e5d77a7d34
mkfizi <mkfizi93@gmail.com>
2022-09-15 12:29:35 +0800

    % typography: ^rename component tags

--- resources/views/dashboard/typography.blade.php
+++ resources/views/dashboard/typography.blade.php
@@ -2,7 +2,7 @@
 
     {{-- Styles --}}
     <div class="mb-4">
-        <x-title>{{ __('Styles') }} </x-title>
+        <x-title-sm>{{ __('Styles') }} </x-title-sm>
     </div>
     <div class="p-4 mb-8 bg-white rounded-md shadow-md dark:bg-gray-800">
         <x-text>{{ __('Font: Poppins') }}</x-text>
@@ -13,40 +13,40 @@
 
     {{-- Titles --}}
     <div class="mb-4">
-        <x-title>{{ __('Titles') }} </x-title>
+        <x-title-sm>{{ __('Titles') }} </x-title-sm>
     </div>
     <div class="p-4 mb-8 bg-white rounded-md shadow-md dark:bg-gray-800">
-        <x-title size="xl">{{ __('Extra Large Title') }}</x-title>
-        <x-title size="lg">{{ __('Large Title') }}</x-title>
-        <x-title size="md">{{ __('Medium Title') }}</x-title>
-        <x-title size="sm">{{ __('Small Title') }}</x-title>
+        <x-title-xl>{{ __('Extra Large Title') }}</x-title-xl>
+        <x-title-lg>{{ __('Large Title') }}</x-title-lg>
+        <x-title-md>{{ __('Medium Title') }}</x-title-md>
+        <x-title-sm>{{ __('Small Title') }}</x-title-sm>
     </div>
 
     {{-- Headings --}}
     <div class="mb-4">
-        <x-title>{{ __('Headings') }} </x-title>
+        <x-title-sm>{{ __('Headings') }} </x-title-sm>
     </div>
     <div class="p-4 mb-8 bg-white rounded-md shadow-md dark:bg-gray-800">
-        <x-heading tag="h1">{{ __('This is a H1 text.') }}</x-heading>
-        <x-heading tag="h2">{{ __('This is a H2 text.') }}</x-heading>
-        <x-heading tag="h3">{{ __('This is a H3 text.') }}</x-heading>
-        <x-heading tag="h4">{{ __('This is a H4 text.') }}</x-heading>
-        <x-heading tag="h5">{{ __('This is a H5 text.') }}</x-heading>
-        <x-heading tag="h6">{{ __('This is a H6 text.') }}</x-heading>
+        <x-heading-1>{{ __('This is a H1 text.') }}</x-heading-1>
+        <x-heading-2>{{ __('This is a H2 text.') }}</x-heading-2>
+        <x-heading-3>{{ __('This is a H3 text.') }}</x-heading-3>
+        <x-heading-4>{{ __('This is a H4 text.') }}</x-heading-4>
+        <x-heading-5>{{ __('This is a H5 text.') }}</x-heading-5>
+        <x-heading-6>{{ __('This is a H6 text.') }}</x-heading-6>
     </div>
 
     {{-- Links --}}
     <div class="mb-4">
-        <x-title>{{ __('Links') }} </x-title>
+        <x-title-sm>{{ __('Links') }} </x-title-sm>
     </div>
     <div class="p-4 mb-8 bg-white rounded-md shadow-md dark:bg-gray-800">
-        <x-link href="#" underline="false">{{ __('This is a link text.') }}</x-link>
-        <x-link href="#" underline="true">{{ __('This is a link text with underline.') }}</x-link>
+        <x-link href="#">{{ __('This is a link text.') }}</x-link>
+        <x-link-underline href="#">{{ __('This is a link text with underline.') }}</x-link-underline>
     </div>
 
     {{-- Lists --}}
     <div class="mb-4">
-        <x-title>{{ __('Lists') }} </x-title>
+        <x-title-sm>{{ __('Lists') }} </x-title-sm>
     </div>
     <div class="p-4 mb-8 bg-white rounded-md shadow-md dark:bg-gray-800">
         <div class="grid gap-4 md:grid-cols-3">
@@ -54,7 +54,7 @@
                 <div class="mb-2">
                     <x-text class="font-semibold">{{ __('Regular List') }}</x-text>
                 </div>
-                <x-list type="none">
+                <x-list>
                     <x-list-item>{{ __('First item.') }}</x-list-item>
                     <x-list-item>{{ __('Second item.') }}</x-list-item>
                     <x-list-item>{{ __('Third item.') }}</x-list-item>
@@ -64,21 +64,21 @@
                 <div class="mb-2">
                     <x-text class="font-semibold">{{ __('Disc List') }}</x-text>
                 </div>
-                <x-list type="disc">
+                <x-list-disc>
                     <x-list-item>{{ __('First item.') }}</x-list-item>
                     <x-list-item>{{ __('Second item.') }}</x-list-item>
                     <x-list-item>{{ __('Third item.') }}</x-list-item>
-                </x-list>
+                </x-list-disc>
             </div>
             <div>
                 <div class="mb-2">
                     <x-text class="font-semibold">{{ __('Numbered List') }}</x-text>
                 </div>
-                <x-list type="decimal">
+                <x-list-decimal>
                     <x-list-item>{{ __('First item.') }}</x-list-item>
                     <x-list-item>{{ __('Second item.') }}</x-list-item>
                     <x-list-item>{{ __('Third item.') }}</x-list-item>
-                </x-list>
+                </x-list-decimal>
             </div>
         </div>
     </div>
