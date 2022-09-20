2921c082046a738a1701cc97c8e12376011539fb
mkfizi <mkfizi93@gmail.com>
2022-09-20 23:24:58 +0800

    @ layouts: -default component & view

--- resources/views/layouts/default.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('app')
-
-@section('layout')
-    <main class="container px-4 py-16 mx-auto">
-        <section class="max-w-md mx-auto mt-48 text-center">
-            {{ $slot }}
-        </section>
-    </main>
-@endsection
