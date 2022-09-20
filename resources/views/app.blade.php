03110afff7b8b78c2d110441ce7ea0b2fd759266
mkfizi <mkfizi93@gmail.com>
2022-09-20 23:26:16 +0800

    % layouts: ^app use section instead of slot

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,10 +7,22 @@
     @include('layouts.partials.styles')
     @include('layouts.partials.scripts')
         
-    <title>{{ !empty($attributes['pageTitle']) ? $attributes['pageTitle'] .' | ' . __('Larastart') : __('Larastart') }}</title>
+    <title>
+        @hasSection('title')
+            {{ config('larastart.name') }} | @yield('title')
+        @else
+            {{ config('larastart.name') }}
+        @endif
+    </title>
 </head>
 
-<body class="relative min-h-screen antialiased text-gray-900 dark:text-white dark:bg-gray-900 {{ isset($bgColor) ? $bgColor  : 'bg-white' }}">
+<body class="relative min-h-screen antialiased text-gray-900 dark:text-white dark:bg-gray-900
+    @hasSection('bgColor')
+        @yield('bgColor')
+    @else
+        bg-white
+    @endif
+">
     
     {{-- Page Layout --}}
     @yield('layout')
