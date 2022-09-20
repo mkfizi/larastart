b99b6635245cc1d6a720527eadb9b2bf2660ddd4
mkfizi <mkfizi93@gmail.com>
2022-09-20 23:25:31 +0800

    % layouts: ^web footer app name

--- resources/views/layouts/partials/web/footer.blade.php
+++ resources/views/layouts/partials/web/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="absolute bottom-0 w-full text-center">
-    <x-text><small>{{ __('Larastart') }} © {{ date('Y')}}</small></x-text>
+    <x-text><small>{{ config('larastart.name') }} © {{ date('Y')}}</small></x-text>
 </footer>
